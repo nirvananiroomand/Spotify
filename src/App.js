f070df4b110871dcb48ecbf0b3a51264755c3ec3
nirvananiroomand <nirvana.niroomand@gmail.com>
2023-08-16 14:58:12 +0430

    initialization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './custom-bootstrap.scss'
+import Playlist from './pages/playlist';
+import RootLayout from './layouts/rootLayout';
+import Album from './pages/album';
+import Artist from './pages/artist';
+import Track from './pages/track';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout/>,
+    children: [
+      {path: '/', element: <HomePage/>},
+      {path: 'albums/:albumName', element: <Album/>},
+      {path: '/playlists/:playlistId', element: <Playlist/>},
+      {path: '/artists/:artistName', element: <Artist/>},
+      {path: '/track/:trackId', element: <Track/>}
+    ],
+  },
+]);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <RouterProvider router={router}/>
   );
 }
 
