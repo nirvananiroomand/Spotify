6007e0daf0c0048deedd7db33c042087db66e3af
nirvananiroomand <nirvana.niroomand@gmail.com>
2023-08-25 20:50:06 +0430

    change images

--- src/components/playlistPreview.js
+++ src/components/playlistPreview.js
@@ -8,13 +8,12 @@ const PlaylistPreview = (props) => {
     return ( 
         <Link to={`/playlists/${playlistId}`} className='text-decoration-none'>
             <div className="card w-auto mx-2 bg-dark-subtle" style={{width: "200px"}}>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSij4xw9-Z1PFYNGcfs1eZtSx55zLOtlYkZkA&usqp=CAU" className="card-img-top" alt="..."/>
+                <img src={`https://picsum.photos/200/?random=${playlistId}`} className="card-img-top" alt="..."/>
                 <div className="card-body" style={{height: "100px"}}>
                     <h5 className="card-title card-text fw-bold" style={{color: "#FEFEFE"}}>{playlistName}</h5>
                     <p className="card-text custom-truncate fw-bold" style={{color: "#646464"}}>
                         {playlistDescription}
                     </p>
-                    {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
                 </div>
             </div>
             <div className="d-flex flex-column">
